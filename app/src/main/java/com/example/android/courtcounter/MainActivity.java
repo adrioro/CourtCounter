ea3df9edd6dede6bcef5c7e5533655d0632796c6
adrioro <artistsvillage@me.com>
2017-06-06 09:06:55 -0400

    Button 1 interaction

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,43 +1,87 @@
 package com.example.android.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.NumberFormat;
 
+import static android.R.id.message;
+
+
+public class MainActivity extends AppCompatActivity {
     int score = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(9);
+
     }
+
+
+    public boolean onOptionsItemSelected(Menu menu) {
+        //Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Handle action bar item clicks here. The action bar will
+        //automatically handle clicks on the Home/Up button, so long
+        //as you specify a parent activity in AndroidManifest.sml.
+        int id = item.getItemId();
+        //noinspection SimplifiableStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     /**
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.display_score);
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
     /**
-     * This method is called when the button_3points button is clicked.
+     * This method is called when the buttonA is clicked.
      */
-    public void button_3points(View view) {
-        String scoreMessage = "score =" + (score * 3);
-        displayMessage(scoreMessage);
+    public void sendMessage(View view) {
+        Integer scoreA = score * 3;
+        displayScore(scoreA);
     }
 
     /**
-     * This method displays the given score value on the screen.
+     * This method displays the given score on the screen.
      */
 
-    private void display_score(int number) {
-        TextView scoreTextView = (TextView) findViewById(R.id.display_score);
-        scoreTextView.setText("" + number);
+    public void displayScore (int number) {
+        TextView txt2 = (TextView) findViewById(R.id.text2);
+        txt2.setText(NumberFormat.getCurrencyInstance().format(number));
+
     }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView txt2 = (TextView) findViewById(R.id.text2);
+        txt2.setText(message);
+
+    }
+
+
 }
+
+
+
