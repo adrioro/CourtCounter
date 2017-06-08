340e7f13c8e0f6032de89ee1eacadcd62cb2ed01
adrioro <artistsvillage@me.com>
2017-06-08 15:40:17 -0400

    Reset Button Added, with interaction.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -11,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
     int scoreTeamB = 0;
     TextView txt2;
+    TextView txt1;
+    int reset = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +22,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * HERE STARTS SPECIFIC CODE FOR TEAM A
-     * <p>
-     * This method is called when the button +3Points is clicked.
+    /*
+     HERE STARTS SPECIFIC CODE FOR TEAM A
      */
+
+
+     /* This method is called when the button +3Points is clicked.
+     */
+
     public void TeamAScores3ForA(View view) {
         scoreTeamA = scoreTeamA + 3;
         displayScoreForA(scoreTeamA);
     }
 
-    /**
-     * This method is called when the button +2Points is clicked.
+    /*
+     This method is called when the button +2Points is clicked.
      */
     public void TeamAScores2ForA(View view) {
         scoreTeamA = scoreTeamA + 2;
@@ -55,40 +61,63 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * HERE STARTS SPECIFIC CODE FOR TEAM B
-     * <p>
-     * /**
-     * This method is called when the button +3Points is clicked.
+    /*
+     HERE STARTS SPECIFIC CODE FOR TEAM B
+     */
+
+     /* This method is called when the button +3Points is clicked.
      */
     public void TeamAScores3ForB(View view) {
         scoreTeamB = scoreTeamB + 3;
         displayScoreForB(scoreTeamB);
     }
 
-    /**
-     * This method is called when the button +2Points is clicked.
+    /*
+     This method is called when the button +2Points is clicked.
      */
     public void TeamAScores2ForB(View view) {
         scoreTeamB = scoreTeamB + 2;
         displayScoreForB(scoreTeamB);
     }
 
-    /**
-     * This method is called when the button button_free is clicked.
+    /*
+     This method is called when the button button_free is clicked.
      */
     public void TeamAScores1ForB(View view) {
         scoreTeamB = scoreTeamB + 1;
         displayScoreForB(scoreTeamB);
     }
 
-    /**
-     * This method displays the given score for Team B on the screen.
+    /*
+     This method displays the given score for Team B on the screen.
      */
 
     public void displayScoreForB(int number) {
         TextView txt2 = (TextView) findViewById(R.id.text2B);
         txt2.setText(String.valueOf(number));
+
+    }
+
+    /*
+     CODE FOR RESER BUTTON
+     */
+
+
+    /* This method is called when the button Reset is clicked.
+     */
+
+    public void ResetAllScores(View view) {
+        displayReset(reset);
+
+    }
+
+    public void displayReset(int number) {
+        TextView txt2 = (TextView) findViewById(R.id.text2B);
+        txt2.setText(String.valueOf(number));
+        TextView txt1 = (TextView) findViewById(R.id.text2A);
+        txt1.setText(String.valueOf(number));
+        scoreTeamA = 0;
+        scoreTeamB = 0;
 
     }
 
